52b7dfae12aa89812594ecdba45ccac3ed65a201
xueliang <xueliang@szusoft.com>
2019-08-23 14:34:07 +0800

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,5 @@
 let user = require("../dao/users");
-let count = 82155;
+let count = 0;
 function add(params, cb){
     user.insert(params, function (err, data) {
         if (err) {
@@ -16,7 +16,7 @@ setInterval(() => {
         "gender": 1,
         "name": name,
         "age": 26,
-        "avatar": "http://192.1.6.55:9101/public/images/2019/7/26/1e7b6074-c9df-474e-b0f1-213b5ae7b4b5.png",
+        "avatar": "http://192.168.1.49:9101/public/images/2019/8/6/8e27b47a-6e19-4803-9d9a-51eb7ed54d16.png",
     }, function(){
             
     })
