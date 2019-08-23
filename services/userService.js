1ea017b357f56eeb93df3626de324f305c2468cc
xueliang <1058392650@qq.com>
2019-08-23 21:57:45 +0800

    Update userService.js

--- services/userService.js
+++ services/userService.js
@@ -1,5 +1,5 @@
 let user = require("../dao/users");
-let count = 82155;
+let count = 0;
 function add(params, cb){
     user.insert(params, function (err, data) {
         if (err) {
@@ -16,7 +16,7 @@ setInterval(() => {
         "gender": 1,
         "name": name,
         "age": 26,
-        "avatar": "http://192.1.6.55:9101/public/images/2019/7/26/1e7b6074-c9df-474e-b0f1-213b5ae7b4b5.png",
+        "avatar": "http://localhost:3002/images/2019/8/3/88cffcf8-daec-42d1-99dd-bd4ce2b68f6e.jpg",
     }, function(){
             
     })
