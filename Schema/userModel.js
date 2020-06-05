16047a2652d96a28edf556ff227cfdcf346df04b
xueliang <xueliang@szusoft.com>
2020-06-05 11:29:15 +0800

    jiegou

--- Schema/userModel.js
+++ Schema/userModel.js
@@ -8,35 +8,38 @@ let Schema = mongoose.Schema; //  创建模型
  *  default：默认值
  */
 let userSchema = new Schema({
-    name: {
-        type: String,
-        index: true
-    },
-    password: String,
-    age: Number,
-    avatar: String,
-    gender: {
-        type: Number,
-        default: 1 // 1 男  2 女
-    },
-    telphone: {
-        type: String,
-        index: true
-    },
-    account: {
-        type: String,
-    },
-    role: {
-        type: String,
-    },
-    dept: {
-        type: String,  
-    },
-    status: {
-        type: Number,
-        default: 1
-    },
-    createtime: { type: Date, default: Date.now },
+	name: {
+		type: String,
+		index: true,
+	},
+	password: {
+		type: String,
+		default: "123456",
+	},
+	age: Number,
+	avatar: String,
+	gender: {
+		type: Number,
+		default: 1, // 1 男  2 女
+	},
+	telphone: {
+		type: String,
+		index: true,
+	},
+	account: {
+		type: String,
+	},
+	role: {
+		type: String,
+	},
+	dept: {
+		type: String,
+	},
+	status: {
+		type: Number,
+		default: 1,
+	},
+	createtime: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('users', userSchema);
--- services/userService.js
+++ services/userService.js
@@ -1,29 +1,31 @@
-let user = require("../dao/users");
+let user = require('../dao/users');
 let count = 0;
-function add(params, cb){
-    user.insert(params, function (err, data) {
-        if (err) {
-            cb(err)
-        } else {
-            cb(null, data)
-        }
-    })
+function add(params, cb) {
+	user.insert(params, function (err, data) {
+		if (err) {
+			cb(err);
+		} else {
+			cb(null, data);
+		}
+	});
 }
 
 let timer = setInterval(() => {
-    let name = `jack${count}`;
-    add({
-        "gender": 1,
-        "name": name,
-        "age": 26,
-        "telphone": "13689784512",
-        "account": "",
-        "role": "",
-        "dept": "", //http://localhost:9101/public/images/2019/10/10/bcea2939-2fcc-4602-bda8-0835ed9c5b92.png
-        "avatar": "http://localhost:9101/public/images/2019/8/6/6bf9def0-61d7-43aa-b7ef-c0b064016943.png",
-    }, function(){
-            
-    })
-    ++count
-},100)
-
+	let name = `jack${count}`;
+	add(
+		{
+			name: name,
+			age: 26,
+			avatar:
+				'http://122.51.12.951:9101/public/images/2019/8/6/6bf9def0-61d7-43aa-b7ef-c0b064016943.png',
+			gender: 1,
+			telphone: '',
+			account: '',
+			role: '',
+			dept: '',
+			status: 1
+		},
+		function () {}
+	);
+	++count;
+}, 100);
